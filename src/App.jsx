4b743b1731654e2cedca46541646590ecf6c62fe
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2023-11-05 19:27:37 +0000

    removed theatre js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import {Canvas, useFrame} from "@react-three/fiber";
+import {Canvas} from "@react-three/fiber";
 import './App.scss';
-import {Environment, OrbitControls, ScrollControls, useScroll} from "@react-three/drei";
+import {Environment, GizmoHelper, GizmoViewport, OrbitControls, ScrollControls, StatsGl} from "@react-three/drei";
 import {Clocks} from "./Clocks.jsx";
 import {createContext, useState} from "react";
-import {getProject, val} from "@theatre/core";
-import {PerspectiveCamera, SheetProvider, useCurrentSheet} from "@theatre/r3f";
+import {useControls} from "leva";
 
 export const rootContext = createContext({
   helpers: false,
@@ -12,15 +11,14 @@ export const rootContext = createContext({
 });
 
 function App() {
-  // const controls = useControls({
-  //   helpers: false,
-  //   wireframe: {
-  //     value: false,
-  //     render: (get) => get('helpers'),
-  //   },
-  // });
+  const controls = useControls({
+    helpers: false,
+    wireframe: {
+      value: false,
+      render: (get) => get('helpers'),
+    },
+  });
   const [stage, setStage] = useState('start');
-  const sheet = getProject('clocks').sheet('scene');
 
   return <>
     {/*<Interface onBtnClick={stageName => setStage(stageName)}/>*/}
@@ -28,54 +26,38 @@ function App() {
       preserveDrawingBuffer: true,
     }}>
 
-      {/*{controls.helpers && <>*/}
-      {/*  <gridHelper/>*/}
-      {/*  <gridHelper rotation-x={Math.PI / 2} position-y={0}/>*/}
-      {/*  <Stats/>*/}
-      {/*  <StatsGl/>*/}
-      {/*  <GizmoHelper alignment="bottom-right" margin={[100, 100]}>*/}
-      {/*    <GizmoViewport labelColor="white" axisHeadScale={1}/>*/}
-      {/*  </GizmoHelper>*/}
-      {/*  <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={controls.helpers} makeDefault/>*/}
-      {/*</>}*/}
-      <ScrollControls pages={100} damping={.4}>
-        <SheetProvider sheet={sheet}>
-          <Scene/>
-        </SheetProvider>
+      {controls.helpers && <>
+        <gridHelper/>
+        <gridHelper rotation-x={Math.PI / 2} position-y={0}/>
+        <Stats/>
+        <StatsGl/>
+        <GizmoHelper alignment="bottom-right" margin={[100, 100]}>
+          <GizmoViewport labelColor="white" axisHeadScale={1}/>
+        </GizmoHelper>
+        <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={controls.helpers} makeDefault/>
+      </>}
+      <ScrollControls pages={10} damping={.4}>
+        <Scene/>
       </ScrollControls>
-
-      {/*<Environment background>*/}
-      {/*  <Lightformer intensity={40} color={'#ffffff'} rotation-y={Math.PI / 2} position={[5, 1, 1]} scale={[20, 1, 1]}/>*/}
-      {/*  <Lightformer intensity={10} rotation-y={Math.PI / 2} position={[-5, 4, -1]} scale={[20, 0.9, 1]}/>*/}
-      {/*  <Lightformer intensity={60} rotation-y={Math.PI / 2} position={[10, 10, 10]} scale={[20, 1, 1]}/>*/}
-      {/*</Environment>*/}
-
     </Canvas>
   </>
 }
 
 function Scene() {
-  const sheet = useCurrentSheet();
-  const scroll = useScroll();
-
-  useFrame(() => {
-    const sequenceLength = val(sheet.sequence.pointer.length);
-    console.log({sequenceLength, offset: scroll.offset, position: sheet.sequence.position, sequence: sheet.sequence});
-    sheet.sequence.position = scroll.offset * sequenceLength;
-  })
 
   return <>
     <directionalLight position={[0, 7, 5]} intensity={5} theatreKey={'light'}/>
-    <PerspectiveCamera theatreKey={'camera'}
-                       makeDefault
-                       position={[0, 0, 3]}
-                       fov={90}
-                       near={0.1}/>
 
     <group position={[0, 0, 0]} theatreKey={'Clocks'}>
       <Clocks/>
     </group>
     <Environment preset="studio"/>
+
+    {/*<Environment background>*/}
+    {/*  <Lightformer intensity={40} color={'#ffffff'} rotation-y={Math.PI / 2} position={[5, 1, 1]} scale={[20, 1, 1]}/>*/}
+    {/*  <Lightformer intensity={10} rotation-y={Math.PI / 2} position={[-5, 4, -1]} scale={[20, 0.9, 1]}/>*/}
+    {/*  <Lightformer intensity={60} rotation-y={Math.PI / 2} position={[10, 10, 10]} scale={[20, 1, 1]}/>*/}
+    {/*</Environment>*/}
   </>
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,6 @@
 import {createRoot} from 'react-dom/client'
 import App from "./App.jsx";
-import studio from '@theatre/studio';
-import extension from '@theatre/r3f/dist/extension';
 import {Suspense} from "react";
-
-studio.extend(extension);
-studio.initialize();
 
 createRoot(document.getElementById('root')).render(
   <Suspense fallback={null}>
